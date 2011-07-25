056790b17a2386f25fef57eca905d45ee357d974
Steve Brown <steve@zergsoft.com>
2011-07-25 14:50:12 +0900

    added options for nonexistent image, flavor etc.  Added Glance connection to base

--- base.py
+++ base.py
@@ -15,11 +15,12 @@
 
 import os
 import random
+import string
 import urlparse
 
 import dtest
+from glance import client as glanceclient
 import novaclient
-
 
 FLAGS = None
 
@@ -76,14 +77,28 @@ def add_opts(opts):
                     default=1,
                     help="ID of flavor for built instances "
                     "[default %default].")
+    opts.add_option("--nonexistent-flavor",
+                    action="store", type="int", dest="nonexistent_flavor",
+                    default=27,
+                    help="ID of nonexistent flavor "
+                    "[default %default].")
     opts.add_option("--image",
                     action="store", type="int", dest="image",
                     default=3,
                     help="ID of image for built instances [default %default].")
+    opts.add_option("--nonexistent-image",
+                    action="store", type="int", dest="nonexistent_image",
+                    default=99999999,
+                    help="ID of nonexistent image [default %default].")
     opts.add_option("--test-image",
                     action="store", type="string", dest="test_image",
                     default="test_image.img",
                     help="Image to use for basic image tests "
+                    "[default %default].")
+    opts.add_option("--bad-test-image",
+                    action="store", type="string", dest="bad_test_image",
+                    default="bad_test_image.img",
+                    help="Image to use for broken image tests "
                     "[default %default].")
     opts.add_option("--stress",
                     action="store_true", dest="stress",
@@ -154,15 +169,26 @@ class BaseIntegrationTest(dtest.DTestCase):
 
         return os
 
+    @staticmethod
+    def getGlanceConnection():
+        """Set up and return a Glance connection."""
+
+        # Set up the Glance connection
+        return glanceclient.Client(FLAGS.glance_host,
+                                   FLAGS.glance_port)
+
     def setUp(self):
         """For each test, set up an OpenStack instance."""
 
         # Get an OpenStack instance
         self.os = self.getOpenStack()
 
+        # Get a Glance connection
+        self.glance_connection = self.getGlanceConnection()
+
     @staticmethod
-    def randName(length=20, charset="abcdefghijklmnopqrstuvwxyz"):
+    def randName(length=20, charset=string.lowercase, prefix=""):
         """Generate a random name of the given length."""
 
-        return ''.join([charset[random.randrange(len(charset))]
+        return prefix + ''.join([charset[random.randrange(len(charset))]
                         for i in xrange(length)])
